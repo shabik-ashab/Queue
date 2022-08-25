9aa4e0e3355507429bfa339c1b26df3ac3a86b16
nub69 <shabik.ashab@gmail.com>
2022-08-25 16:43:19 +0600

    front back empty adeed

--- MYQUEUE.h
+++ MYQUEUE.h
@@ -39,13 +39,38 @@ public:
 
     //dequeue ---> pop
 
+    int pop(){
+        int chk = -1;
+        if(rear == NULL){
+            cout<<"queue underflow || there is no element"<<endl;
+            return -1;
+        }
+
+        Node* delNode;
+        delNode = front;
+
+        front = front->next;
+        if(front == NULL) rear = NULL;
+        chk = delNode->value;
+
+        delete delNode;
+        return chk;
+    }
+
     //peel ---> front() back()
+    int front(){
+        int chk = front->value;
+        return chk;
+    }
+
+    int back(){
+        int chk = rear->value;
+        return chk;
+    }
 
     //empty
+    bool empty(){
+        if(front == NULL && rear == NULL) return true;
+        else return false;
+    }
 };
-
-int main()
-{
-    
-    return 0;
-}
