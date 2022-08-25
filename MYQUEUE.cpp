5311b18b23da480cc1463aa9ffab88762cf4e219
nub69 <shabik.ashab@gmail.com>
2022-08-25 16:32:06 +0600

    Node and Queue classs initialized

---
+++ MYQUEUE.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    int value;
+    Node* next;
+
+public:
+
+    Node(int val){
+        value = val;
+        next = NULL;
+    }
+
+};
+
+class Queue{
+    Node* front;
+    Node* rear;
+
+public:
+    Queue(){
+        front = NULL;
+        rear = NULL;
+    }
+
+    //
+};
+
+int main()
+{
+    
+    return 0;
+}
